f373b605851ae0493952d6303450f4ea04e555a1
Rafael Birochi Cerri <rafaelbcerri@hotmail.com>
2018-04-08 14:26:08 -0300

    Fix indentation

--- src/containers/feed.jsx
+++ src/containers/feed.jsx
@@ -5,31 +5,31 @@ import PostCard from "../components/postCard.jsx";
 import EditCard from "../components/editCard.jsx";
 
 class Feed extends React.Component {
-    render() {
-        return (
-          <div>
-            <div className="row">
-              <div className="col m6 offset-m3">
-                <EditCard editCardId="postCard" textValue=""/>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col m2 offset-m7">
-                <Dropdown classes="col s12" dropdownId="filter" options={[{value: "friends", text: "Amigos"}, {value: "public", text: "Público"}]} label="Filtro" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col m6 offset-m3">
-                <PostCard text="Oi tudo bem com você?"/>
-                <PostCard text="Oi tudo bem com você?"/>
-                <PostCard text="Oi tudo bem com você?"/>
-                <PostCard text="Oi tudo bem com você?"/>
-                <PostCard text="Oi tudo bem com você?"/>
-              </div>
-            </div>
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col m6 offset-m3">
+            <EditCard editCardId="postCard" textValue=""/>
           </div>
-        );
-    }
+        </div>
+        <div className="row">
+          <div className="col m2 offset-m7">
+            <Dropdown classes="col s12" dropdownId="filter" options={[{value: "friends", text: "Amigos"}, {value: "public", text: "Público"}]} label="Filtro" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col m6 offset-m3">
+            <PostCard text="Oi tudo bem com você?"/>
+            <PostCard text="Oi tudo bem com você?"/>
+            <PostCard text="Oi tudo bem com você?"/>
+            <PostCard text="Oi tudo bem com você?"/>
+            <PostCard text="Oi tudo bem com você?"/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 
--- src/containers/login.jsx
+++ src/containers/login.jsx
@@ -5,21 +5,21 @@ import CardWrapper from "../components/cardWrapper.jsx";
 import FlatButton from "../components/flatButton.jsx";
 
 class Login extends React.Component {
-    render() {
-        return (
-          <div className="row">
-            <div className="col m6 offset-m3">
-              <CardWrapper title="Login">
-                <div>
-                  <Input id="email" label="Email" type="text" icon="email" />
-                  <Input id="password" label="Senha" type="password" icon="lock" />
-                </div>
-                <FlatButton text="Entrar" classes="teal-text"/>
-              </CardWrapper>
+  render() {
+    return (
+      <div className="row">
+        <div className="col m6 offset-m3">
+          <CardWrapper title="Login">
+            <div>
+              <Input id="email" label="Email" type="text" icon="email" />
+              <Input id="password" label="Senha" type="password" icon="lock" />
             </div>
-          </div>
-        );
-    }
+            <FlatButton text="Entrar" classes="teal-text"/>
+          </CardWrapper>
+        </div>
+      </div>
+    );
+  }
 }
 
 
